3dd078e36be20d052c85c69ec7093db2b8ac8c7f
PrestonWang <PrestonWang@users.noreply.github.com>
2024-04-19 00:35:29 +0000

    deploy: 7a48907841185b00105401d8df1848600111d3e7

--- js/daily-bread-widget/helpers.ts
+++ js/daily-bread-widget/helpers.ts
@@ -9,3 +9,18 @@ export const localizedDateFormat = (date: Date): string => {
     const year = date.getFullYear();
     return `${month} ${day}, ${year}`;
 };
+
+export const AWSDateFormat = (date: Date):string=>{
+        const d = new Date(date);
+        let month = '' + (d.getMonth() + 1);  // getMonth() returns months from 0-11
+        let day = '' + d.getDate();
+        const year = d.getFullYear();
+      
+        // Pad the month and day with leading zeros if necessary
+        if (month.length < 2) 
+          month = '0' + month;
+        if (day.length < 2) 
+          day = '0' + day;
+      
+        return [year, month, day].join('-');
+}
